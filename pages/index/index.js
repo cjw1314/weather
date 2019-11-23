0528a4625eec77b57f6698bf1baab24b48530edd
Mrkepler <cjwcjw1314@163.com>
2019-11-23 23:04:25 +0800

    unfinished

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,37 @@
-Page({})
+Page({
+data:{
+nowTemp:'14℃',
+nowWeather:'阴天'
+
+},
+
+
+onLoad(){
+    console.log("hello world");
+    wx.request({
+        url: 'https://test-miniprogram.com/api/weather/now',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        data:{
+            city:'广州市'
+        },
+        success: res=> {
+            console.log(res.data);
+          let result=res.data.result;
+          let temp = result.now.temp;
+          let weather=result.now.weather;
+          console.log(weather,temp);
+          this.setData({
+            nowTemp:temp,
+            nowWeather:weather
+          })
+        }
+    })
+}
+
+
+
+
+
+})
