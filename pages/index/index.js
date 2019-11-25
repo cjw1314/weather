2066871e6bc7bd5daeb047bbaef736cf19a3d570
Mrkepler <cjwcjw1314@163.com>
2019-11-25 22:33:29 +0800

    11.25

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,59 +1,72 @@
 const weatherMap = {
-    'sunny': '晴天',
-    'cloudy': '多云',
-    'overcast': '阴',
-    'lightrain': '小雨',
-    'heavyrain': '大雨',
-    'snow': '雪'
-  }
-  const weatherColorMap = {
-    'sunny': '#cbeefd',
-    'cloudy': '#deeef6',
-    'overcast': '#c6ced2',
-    'lightrain': '#bdd5e1',
-    'heavyrain': '#c5ccd0',
-    'snow': '#aae1fc'
-   }
-
-Page({
-data:{
-nowTemp:'14℃',
-nowWeather:'阴天',
-nowWeatherBackground:''
-},
-
-
-onLoad(){
-    console.log("hello world");
-    wx.request({
-        url: 'https://test-miniprogram.com/api/weather/now',
-        header: {
-            'Content-Type': 'application/json'
-        },
-        data:{
-            city:'广州市'
-        },
-        success: res=> {
-            console.log(res.data);
-          let result=res.data.result;
-          let temp = result.now.temp;
-          let weather=result.now.weather;
-          console.log(weather,temp);
-          this.setData({
-            nowTemp:temp+'℃',
-            nowWeather:weatherMap[weather],
-            nowWeatherBackground:'../../bgp/'+weather+'-bg.png'
-            
-          })
-          wx.setNavigationBarColor({
-            frontColor: '#000000',
-            backgroundColor: weatherColorMap[weather],
-     })
-        }
-    })
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
 }
 
+Page({
+  data: {
+    nowTemp: '14℃',
+    nowWeather: '阴天',
+    nowWeatherBackground: '',
+    forcast:[1,2,32,5,4564,65,456,46,654,564,56,46,456]
+  },
 
+
+  onPullDownRefresh() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh();
+    })
+
+  },
+
+  onLoad() {
+    this.getNow();
+
+  },
+
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        console.log(res.data);
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        console.log(weather, temp);
+        this.setData({
+          nowTemp: temp + '℃',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '../../bgp/' + weather + '-bg.png'
+
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  }
 
 
 
